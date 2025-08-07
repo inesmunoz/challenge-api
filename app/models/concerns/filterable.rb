4db64b600a94b83e40fd4566d8db292fd15736a8
imunozfi  <ines.munozfigueroa@emeal.nttdata.com>
2025-08-06 21:08:01 -0400

    ADD: arel

--- app/models/concerns/filterable.rb
+++ app/models/concerns/filterable.rb
@@ -2,7 +2,7 @@
 module Filterable
   extend ActiveSupport::Concern
   OPERATORS = { from: ">=", to: "<=" }
-  ALLOWED_FIXED_COLUMNS = %w[created_at update_at]
+  ALLOWED_FIXED_COLUMNS = %w[created_at updated_at]
   class_methods do
     def self.allowed_filter_columns
       ALLOWED_FIXED_COLUMNS | (column_names - ALLOWED_FIXED_COLUMNS)
@@ -19,14 +19,20 @@ module Filterable
         elsif key.to_s.match?(/_(from|to)\z/)
           base_field = key.to_s.gsub(/_(from|to)\z/, "").underscore
 
-          if allowed_filter_columns.include?(base_field)
+         if allowed_filter_columns.include?(base_field)
+            table = self.arel_table
+            arel_column = table[base_field]
 
-            operator = OPERATORS[key.to_s.split("_").last.to_sym]
-            scope = scope.where("#{base_field} #{operator} ?", value)  # :brakeman_skip
-          else
-            Rails.logger.debug "Unknown base_field: #{base_field} for filter #{key}"
-            raise ArgumentError, "Unknown filter: #{key}"
-          end
+            condition = if key.to_s.ends_with?("_from")
+                          arel_column.gteq(value)
+            else
+                          arel_column.lteq(value)
+            end
+
+            scope = scope.where(condition)
+         else
+          Rails.logger.debug "Unknown base_field: #{base_field} for filter #{key}"
+         end
 
         elsif value.is_a?(Array)
           scope = scope.where(key => value)
